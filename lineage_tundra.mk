47d1aabf23ad3e20b137da6293ae87046ca23548
truemodder <rootacessbyuncogn@gmail.com>
2026-03-22 16:12:41 -0300

    Update product configuration for infinity_tundra

--- lineage_tundra.mk
+++ lineage_tundra.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/tundra/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_tundra
+PRODUCT_NAME := infinity_tundra
 PRODUCT_DEVICE := tundra
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
